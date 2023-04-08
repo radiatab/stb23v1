83a7923da8c9077ad0b5a3c9b1afe64eebcdea99
radiiatabet@gmail.com <radiiatabet@gmail.com>
2023-04-08 23:09:31 +0200

    premier commit

---
+++ src/main/java/fr/univrouen/stb23v1/Stb23v1Application.java
@@ -0,0 +1,17 @@
+package fr.univrouen.stb23v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stb23v1Application {
+
+	public static void main(String[] args) {
+		System.getProperties().put("server.port", 8100);
+		SpringApplication.run(Stb23v1Application.class, args);
+		
+	}
+
+}
+
+
