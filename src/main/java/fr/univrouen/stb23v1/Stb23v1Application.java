a180b68193856be57e07c7a7405036afc3e723e0
radiiatabet@gmail.com <radiiatabet@gmail.com>
2023-04-11 15:22:05 +0200

    modifs controlleurs2

--- src/main/java/fr/univrouen/stb23v1/Stb23v1Application.java
+++ src/main/java/fr/univrouen/stb23v1/Stb23v1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Stb23v1Application {
 
 	public static void main(String[] args) {
-		System.getProperties().put("server.port", 8100);
+		System.getProperties().put("server.port", 8080);
 		SpringApplication.run(Stb23v1Application.class, args);
 		
 	}
